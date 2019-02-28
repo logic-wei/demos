279627f4f28867726e3a5453a83f64c92df1435d
logic-wei <logic_wei@163.com>
2019-02-28 22:29:41 +0800

    广播示例添加博客链接

--- AnDemos/app/src/main/java/com/example/weipeng/andemos/BroadcastDemo/BroadcastDemoActivity.java
+++ AnDemos/app/src/main/java/com/example/weipeng/andemos/BroadcastDemo/BroadcastDemoActivity.java
@@ -4,19 +4,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.weipeng.andemos.DemoActivity;
 import com.example.weipeng.andemos.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class BroadcastDemoActivity extends DemoActivity {
 
     private TextView mBatText;
+    private List<Button> mAllButtons = new ArrayList<>();
+    private OnClickListener mOnClickListener = new OnClickListener();
 
     private BroadcastReceiver mMyBroadcastReceiver = new MyBroadcastReceiver();
     private IntentFilter mIntentFilter = new IntentFilter();
@@ -41,6 +48,24 @@ public class BroadcastDemoActivity extends DemoActivity {
 
     private void initViews() {
         mBatText = findViewById(R.id.text_battery);
+        mAllButtons.add((Button) findViewById(R.id.button_blog_broadcast));
+        for (Button button: mAllButtons) {
+            button.setOnClickListener(mOnClickListener);
+        }
+    }
+
+    private class OnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button_blog_broadcast:{
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("https://github.com/logic-wei/blog/blob/master/articles/Android/%E5%9B%9B%E5%A4%A7%E7%BB%84%E4%BB%B6-%E5%B9%BF%E6%92%AD/main.md"));
+                    startActivity(intent);
+                }break;
+            }
+        }
     }
 
     @Override
